51f65cea688ad25f0d800b40e0bec02d91d4c2b4
Rachhen <rachhen.it@outlook.com>
2021-01-06 00:16:56 +0700

    First commit

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function CommentCreate({ postId }) {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post(`http://posts.com/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+            type="text"
+            value={content}
+            className="form-control"
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default CommentCreate;
---
+++ client/src/PostCreate.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function PostCreate() {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post("http://posts.com/posts/create", { title });
+
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            type="text"
+            value={title}
+            className="form-control"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default PostCreate;
